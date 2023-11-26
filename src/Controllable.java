972000d143ef83015a005dde89f6f62372d60d80
Balázs Réka <balazs.reka@ktch.hu>
2023-11-26 18:14:56 +0100

    kötelező adatok hozzáadva minden fájlhoz

--- src/Controllable.java
+++ src/Controllable.java
@@ -1,3 +1,15 @@
+/*
+* File: Controllable.java
+* Author: Balázs Réka
+* Copyright: 2023, Balázs Réka
+* Group: Szoft II/2
+* Date: 2023-11-06
+* Github:https://github.com/BalazsR2022/Dolgozat1.git
+* Licenc: GNU GPL
+*/
+
+
+
 public interface Controllable {
     void start();
     void stop();
--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,17 @@
+/*
+* File: Controller.java
+* Author: Balázs Réka
+* Copyright: 2023, Balázs Réka
+* Group: Szoft II/2
+* Date: 2023-11-06
+* Github:https://github.com/BalazsR2022/Dolgozat1.git
+* Licenc: GNU GPL
+*/
+ 
+
+
+
+
 public class Controller implements Controllable{
 
     @Override
